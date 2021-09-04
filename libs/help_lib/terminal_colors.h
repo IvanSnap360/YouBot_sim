f7dcb41307fb343d468e5bd2639df4507aab5215
Ivan <ivpetr2003@gmail.com>
2021-09-04 13:12:21 +0300

    add sim_libs and cmake file

---
+++ libs/help_lib/terminal_colors.h
@@ -0,0 +1,16 @@
+#define Reset   "\u001b[0m"
+
+// font colors
+#define Black   "\u001b[30m"
+#define Red     "\u001b[31m"
+#define Green   "\u001b[32m"
+#define Yellow  "\u001b[33m"
+#define Blue    "\u001b[34m"
+#define Magenta "\u001b[35m"
+#define Cyan    "\u001b[36m"
+#define White   "\u001b[37m"
+
+// font styles
+#define Bold             "\u001b[1m"
+#define Underline        "\u001b[4m"
+#define Reversed         "\u001b[7m"
---
+++ start.sh
@@ -0,0 +1,21 @@
+read -p "Compile ? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd build
+     
+    make
+
+    cd ..
+fi
+
+read -p "Run ? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd build
+     
+    ./ManipControl
+
+    cd ..
+fi
