94939bf26e7ba35d5ea3f17e090be7d7cd11fea5
IvanSnap360 <ivpetr2003@gmail.com>
2021-10-16 19:28:29 +0300

    add py branch

--- libs/help_lib/print_lib.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "terminal_colors.h"
-#define print(x) std::cout << x;
-#define println(x) std::cout << x << std::endl;
-
-#define INFO_PRINT(x) std::cout << Bold << White  << "[INFO] " << Reset << x << std::endl;
-#define DEBUG_PRINT(x) std::cout << Bold << Green  << "[DEBUG] "<< Reset << x << std::endl;
-#define WARN_PRINT(x) std::cout << Bold << Yellow << "[WARN] " << Reset << x << std::endl;
-#define ERROR_PRINT(x) std::cout << Bold << Red    << "[ERROR] "<< Reset << x << std::endl;
